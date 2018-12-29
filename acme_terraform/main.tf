78809c8df5aeef3feabcc4670e6f1befc6b3d3ca
Greg Piper <7915411+gpiper14@users.noreply.github.com>
2018-12-29 10:53:10 -0500

    acme terraform working

--- acme_terraform/main.tf
+++ acme_terraform/main.tf
@@ -1,9 +1,17 @@
+terraform {
+  backend "local" {}
+}
+
 variable "staging" {
   type = "string"
   default = "true"
 }
 
 variable "email" {
+  type = "string"
+}
+
+variable "common_name" {
   type = "string"
 }
 
@@ -26,7 +34,7 @@ resource "acme_registration" "reg" {
 
 resource "acme_certificate" "certificate" {
   account_key_pem = "${acme_registration.reg.account_key_pem}"
-  common_name = "${element(var.dns_names, 0)}"
+  common_name = "${var.common_name}"
   subject_alternative_names = ["${var.dns_names}"]
 
   dns_challenge {
@@ -36,7 +44,7 @@ resource "acme_certificate" "certificate" {
 
 resource "local_file" "cert_file" {
   filename = "/ssl/fullchain.pem"
-  content = "${acme_certificate.certificate.certificate_pem}"
+  content = "${acme_certificate.certificate.certificate_pem}\n${acme_certificate.certificate.issuer_pem}"
 }
 
 resource "local_file" "key_file" {
--- acme_terraform/run.sh
+++ acme_terraform/run.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 set -e
 
-CERT_DIR=/data/letsencrypt
+STATE_DIR=/data/acme_state
 CONFIG_PATH=/data/options.json
 
 export TF_INPUT=0
 export TF_DATA_DIR='/data/tf_working'
 
 export TF_VAR_email=$(jq --raw-output ".email" $CONFIG_PATH)
-epxort TF_VAR_dns_names=$(jq --raw-output ".domains" $CONFIG_PATH)
-export TF_VAR_key_file=$(jq --raw-output ".keyfile" $CONFIG_PATH)
-export TF_VAR_cert_file=$(jq --raw-output ".certfile" $CONFIG_PATH)
+export TF_VAR_common_name=$(jq --raw-output ".common_name" $CONFIG_PATH)
+export TF_VAR_dns_names=$(jq --raw-output ".domains" $CONFIG_PATH)
 export TF_VAR_staging=$(jq --raw-output ".staging" $CONFIG_PATH)
 
 export AWS_ACCESS_KEY_ID=$(jq --raw-output ".aws_access_key_id" $CONFIG_PATH)
 export AWS_SECRET_ACCESS_KEY=$(jq --raw-output ".aws_secret_access_key" $CONFIG_PATH)
 export AWS_DEFAULT_REGION='us-east-1'
 
-mkdir -p "$CERT_DIR" "${TF_DATA_DIR}"
 
-terraform init
-terraform apply
+mkdir -p "${STATE_DIR}" "${TF_DATA_DIR}"
 
-chmod 0644 "${TF_VAR_cert_file}" "${TF_VAR_key_file}"
+function update_certs(){
+  terraform init -backend-config="path=${STATE_DIR}/acme.tfstate"
+  terraform apply -auto-approve
+  chmod 0644 "/ssl/fullchain.pem" "/ssl/privkey.pem"
+}
 
-
+while true; do
+  update_certs
+  sleep '2d'
+done
